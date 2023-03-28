09d312feb03eb6505cff25a295b42f3361a4ba1c
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-29 03:33:27 +0530

    dynamic rendering

--- Web Development/node/coder-dost-node/index.js
+++ Web Development/node/coder-dost-node/index.js
@@ -1,8 +1,41 @@
-const { sum, diff } = require('./lib')
+const http = require('http')
 const fs = require('fs')
 
-fs.readFile('demo.txt', 'utf-8', (err, txt) => {
-    console.log(txt)
-});
+const index = fs.readFileSync('index.html', 'utf-8')
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+const products = data.products
 
-console.log(sum(4, 5), diff(9, 4))
+// const data = { age: 5 }
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    if (req.url.startsWith('/product')) {
+        const id = req.url.split('/')[2]
+        const product = products.find(p => p.id === (+id))
+        console.log(product)
+        res.setHeader('Content-Type', 'text/html');
+        let modifiedIndex = index.replace('**title**', product.title)
+            .replace('**url**', product.thumbnail)
+            .replace('**price**', product.price)
+            .replace('**rating**', product.rating)
+        res.end(modifiedIndex);
+        return;
+    }
+
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html')
+            res.end(index)
+            break
+        case '/api':
+            res.setHeader('Content-Type', 'Application/json')
+            res.end(JSON.stringify(data))
+            break
+
+        default:
+            res.writeHead(404, 'Page Not Found')
+            res.end()
+    }
+})
+
+server.listen(8000)
